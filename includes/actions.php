84b4726d5a183ea12d1c9585244ed1cfc89aa162
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-10-04 11:30:48 +0530

    fix: set compaign param in GA request if non emtpy

--- includes/actions.php
+++ includes/actions.php
@@ -50,44 +50,61 @@ function give_google_analytics_send_donation_success( $donation_id, $new_status,
 		$ga_categories = give_get_option( 'google_analytics_category', 'Donations' );
 		$ga_list       = give_get_option( 'google_analytics_list' );
 
-		$args = apply_filters(
-			'give_google_analytics_record_offsite_payment_hit_args', array(
-				'v'     => 1,
-				'tid'   => $ua_code, // Tracking ID required.
-				'cid'   => ! empty( $client_id ) ? $client_id : give_analytics_gen_uuid(), // Client ID. Required (Set random if does not find client id ).
-				't'     => 'event', // Event hit type.
-				'ec'    => 'Fundraising', // Event Category. Required.
-				'ea'    => 'Donation Success', // Event Action. Required.
-				'el'    => $form_title, // Event Label.
-				'ti'    => $donation_id, // Transaction ID.
-				'ta'    => $affiliation,  // Affiliation.
-				'cn'    => $campaign,  // Campaign Name.
-				'cs'    => $campaign_source,  // Campaign Source.
-				'cm'    => $cpmpaign_medium,  // Campaign Medium.
-				'cu'    => give_get_payment_currency_code( $donation_id ),  // Currency code.
-				'pal'   => $ga_list,   // Product Action List.
-				'pa'    => 'purchase',
-				'pr1id' => $form_id,  // Product 1 ID. Either ID or name must be set.
-				'pr1nm' => $form_title, // Product 1 name. Either ID or name must be set.
-				'pr1ca' => $ga_categories, // Product 1 category.
-				'pr1br' => 'Fundraising',
-				'pr1qt' => 1, // Product 1 quantity.
-				'pr1pr' => $total, // Product price
-				'tr'    => $total, // Transaction Revenue
-			)
+		$args = array(
+			'v'     => 1,
+			'tid'   => $ua_code, // Tracking ID required.
+			'cid'   => ! empty( $client_id ) ? $client_id : give_analytics_gen_uuid(), // Client ID. Required (Set random if does not find client id ).
+			't'     => 'event', // Event hit type.
+			'ec'    => 'Fundraising', // Event Category. Required.
+			'ea'    => 'Donation Success', // Event Action. Required.
+			'el'    => $form_title, // Event Label.
+			'ti'    => $donation_id, // Transaction ID.
+			'ta'    => $affiliation,  // Affiliation.
+			'cu'    => give_get_payment_currency_code( $donation_id ),  // Currency code.
+			'pal'   => $ga_list,   // Product Action List.
+			'pa'    => 'purchase',
+			'pr1id' => $form_id,  // Product 1 ID. Either ID or name must be set.
+			'pr1nm' => $form_title, // Product 1 name. Either ID or name must be set.
+			'pr1ca' => $ga_categories, // Product 1 category.
+			'pr1br' => 'Fundraising',
+			'pr1qt' => 1, // Product 1 quantity.
+			'pr1pr' => $total, // Product price
+			'tr'    => $total, // Transaction Revenue
 		);
+
+		// Campaign Name.
+		if ( $campaign ) {
+			$args['cn'] = $campaign;
+		}
+
+		// Campaign Source.
+		if ( $campaign_source ) {
+			$args['cs'] = $campaign_source;
+		}
+
+		// Campaign Medium
+		if ( $campaign_medium ) {
+			$args['cm'] = $campaign_medium;
+		}
+
+		/**
+		 * Filter the google analatic query params
+		 *
+		 */
+		$args = apply_filters( 'give_google_analytics_record_offsite_payment_hit_args', $args );
 
 		$args    = array_map( 'rawurlencode', $args );
 		$url     = add_query_arg( $args, 'https://www.google-analytics.com/collect' );
 		$request = wp_remote_post( $url );
 
 		// Check if beacon sent successfully.
-		if ( ! is_wp_error( $request ) || 200 === wp_remote_retrieve_response_code( $request ) ) {
+		if ( ! is_wp_error( $request ) || 200 == wp_remote_retrieve_response_code( $request ) ) {
 
 			add_post_meta( $donation_id, '_give_ga_beacon_sent', true );
 			give_insert_payment_note( $donation_id, __( 'Google Analytics ecommerce tracking beacon sent.', 'give-google-analytics' ) );
 		}
-	} // End if().
+
+	}// End if().
 
 }
 
