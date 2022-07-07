47b16743d41a39953912a54d9487fb31e4d53d06
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-07-07 21:55:21 +0530

    format: improve code formatting

--- assets/src/js/give-ga-settings.js
+++ assets/src/js/give-ga-settings.js
@@ -8,69 +8,69 @@
  */
 
 jQuery.noConflict();
-(function( $ ) {
+(function ($) {
 
-	/**
-	 * Toggle Conditional Form Fields
-	 *
-	 *  @since: 1.0
-	 */
-	const toggle_ga_fields = function() {
+    /**
+     * Toggle Conditional Form Fields
+     *
+     *  @since: 1.0
+     */
+    const toggle_ga_fields = function () {
 
-		var ga_tracking_customize = $( 'input[name="google_analytics_tracking_vals"]' );
+        var ga_tracking_customize = $('input[name="google_analytics_tracking_vals"]');
 
-		ga_tracking_customize.on( 'change', function() {
+        ga_tracking_customize.on('change', function () {
 
-			var ga_tracking_customize_val = $( this ).filter( ':checked' ).val();
+            var ga_tracking_customize_val = $(this).filter(':checked').val();
 
-			if ( 'undefined' === typeof ga_tracking_customize_val ) {
-				return;
-			}
+            if ('undefined' === typeof ga_tracking_customize_val) {
+                return;
+            }
 
-			if ( ga_tracking_customize_val === 'default' ) {
-				$( '.give-ga-advanced-field' ).hide();
-			} else {
-				$( '.give-ga-advanced-field' ).show();
-			}
+            if (ga_tracking_customize_val === 'default') {
+                $('.give-ga-advanced-field').hide();
+            } else {
+                $('.give-ga-advanced-field').show();
+            }
 
-		} ).change();
+        }).change();
 
-	};
+    };
 
     /**
      * Toggle Conditional Google Analytics 4 Form Fields
      *
      *  @unreleased
      */
-    const toggle_google_analytics_4_fields = function() {
+    const toggle_google_analytics_4_fields = function () {
 
-        const ga_tracking_mode = $( 'input[name="google_tracking_mode"]' );
+        const ga_tracking_mode = $('input[name="google_tracking_mode"]');
 
-        ga_tracking_mode.on( 'change', function() {
+        ga_tracking_mode.on('change', function () {
 
-            const ga_tracking_mode_val = $( this ).filter( ':checked' ).val();
+            const ga_tracking_mode_val = $(this).filter(':checked').val();
 
-            if ( 'undefined' === typeof ga_tracking_mode_val ) {
+            if ('undefined' === typeof ga_tracking_mode_val) {
                 return;
             }
 
-            if ( ga_tracking_mode_val === 'universal-analytics' ) {
-                $( '.give-universal-analytics' ).removeClass('give-hidden');
-                $( '.give-google-analytics-4' ).addClass('give-hidden');
+            if (ga_tracking_mode_val === 'universal-analytics') {
+                $('.give-universal-analytics').removeClass('give-hidden');
+                $('.give-google-analytics-4').addClass('give-hidden');
             } else {
-                $( '.give-universal-analytics' ).addClass('give-hidden');
-                $( '.give-google-analytics-4' ).removeClass('give-hidden');
+                $('.give-universal-analytics').addClass('give-hidden');
+                $('.give-google-analytics-4').removeClass('give-hidden');
             }
 
-        } ).change();
+        }).change();
 
     };
 
-	// On DOM Ready
-	$( function() {
+    // On DOM Ready
+    $(function () {
 
-		toggle_ga_fields();
+        toggle_ga_fields();
         toggle_google_analytics_4_fields();
-	} );
+    });
 
-})( jQuery );
+})(jQuery);
