54315d1a27cafb78aa4e65430591b35fda3734c8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-08-02 23:48:46 +0530

    doc: add comments

--- src/Addon/DataTransferObjects/CampaignDTO.php
+++ src/Addon/DataTransferObjects/CampaignDTO.php
@@ -7,8 +7,27 @@ namespace GiveGoogleAnalytics\Addon\DataTransferObjects;
  */
 class CampaignDTO
 {
+    /**
+     * @unreleased
+     * @var string
+     */
     public $campaignName;
+
+    /**
+     * @unreleased
+     * @var string
+     */
     public $campaignSource;
+
+    /**
+     * @unreleased
+     * @var string
+     */
     public $campaignMedium;
+
+    /**
+     * @unreleased
+     * @var string
+     */
     public $campaignContent;
 }
