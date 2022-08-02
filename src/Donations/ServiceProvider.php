41d8ab2be29bd8b2bf733a18b0ec058b8eaa0ef2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-08-02 11:54:59 +0530

    refactor: move code to src dir

--- src/Donations/ServiceProvider.php
+++ src/Donations/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace GiveGoogleAnalytics\Donations;
 
 use Give\Helpers\Hooks;
+use GiveGoogleAnalytics\Donations\Actions\SendEventToGoogleWithUniversalAnalytics;
 use GiveGoogleAnalytics\Donations\Actions\StoreDonorGoogleAnalyticsData;
 
 /**
@@ -27,5 +28,12 @@ class ServiceProvider implements \Give\ServiceProviders\ServiceProvider
     public function boot()
     {
         Hooks::addAction('give_insert_payment', StoreDonorGoogleAnalyticsData::class);
+        Hooks::addAction(
+            'give_update_payment_status',
+            SendEventToGoogleWithUniversalAnalytics::class,
+            '__invoke',
+            110,
+            3
+        );
     }
 }
