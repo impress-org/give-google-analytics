68375c0b5afc09324d2ef4062151bfd1aa6c01e9
Joshua Dinh <josh@joshuadinh.tech>
2023-08-08 10:03:59 -0700

    doc: update tag description

--- src/Donations/ServiceProvider.php
+++ src/Donations/ServiceProvider.php
@@ -23,7 +23,7 @@ class ServiceProvider implements ServiceProviderInterface
     }
 
     /**
-     * @unreleased Add page_view events for v3 forms.
+     * @unreleased Add frontend events for v3 forms.
      * @since 2.0.0
      * @return void
      */
