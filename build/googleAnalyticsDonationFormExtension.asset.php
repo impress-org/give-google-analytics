b48f2b0a3e3fe54d5614b537bced5fe39c3a9a29
Joshua Dinh <josh@joshuadinh.tech>
2023-08-14 10:18:19 -0700

    fix: remove build dir. Include build in .gitignore.

--- build/googleAnalyticsDonationFormExtension.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'c66c2da91ed4468a8ac0');
--- build/googleAnalyticsFormBuilderExtension.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acc4913290fd5a194788');
