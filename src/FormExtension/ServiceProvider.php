42e1d689351f954755d22922094a8ab5da00a7b1
Joshua Dinh <josh@joshuadinh.tech>
2023-08-15 12:47:47 -0700

    feature: hook into givewp_donation_form_schema.

--- src/FormExtension/ServiceProvider.php
+++ src/FormExtension/ServiceProvider.php
@@ -4,11 +4,9 @@ namespace GiveGoogleAnalytics\FormExtension;
 
 use Give\Helpers\Hooks;
 use Give\ServiceProviders\ServiceProvider as ServiceProviderInterface;
-use GiveGoogleAnalytics\FormExtension\Hooks\DonationFormBlockRender;
+use GiveGoogleAnalytics\FormExtension\DonationForm\Actions\ConvertGoogleAnalyticOptionsToField;
 use GiveGoogleAnalytics\FormExtension\Hooks\DonationFormScriptsEnqueue;
-use GiveGoogleAnalytics\FormExtension\Hooks\FormBuilderScriptsEnqueue;
 use GiveGoogleAnalytics\FormExtension\Hooks\GoogleTagScripts;
-use GiveGoogleAnalytics\FormExtension\Hooks\NewFormDefaultBlocks;
 
 /**
  * Class ServiceProvider
@@ -31,15 +29,14 @@ class ServiceProvider implements ServiceProviderInterface
     public function boot()
     {
         // Form Extension hooks
-        Hooks::addAction('givewp_form_builder_new_form', NewFormDefaultBlocks::class);
-        Hooks::addAction('givewp_form_builder_enqueue_scripts', FormBuilderScriptsEnqueue::class);
-        Hooks::addAction('givewp_donation_form_enqueue_scripts', DonationFormScriptsEnqueue::class);
-        Hooks::addFilter(
-            'givewp_donation_form_block_render_givewp-google-analytics/google-analytics',
-            DonationFormBlockRender::class,
+        Hooks::addAction(
+            'givewp_donation_form_schema',
+            ConvertGoogleAnalyticOptionsToField::class,
             '__invoke',
             10,
-            4
+            2
         );
+
+        Hooks::addAction('givewp_donation_form_enqueue_scripts', DonationFormScriptsEnqueue::class);
     }
 }
